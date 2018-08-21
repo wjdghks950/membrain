48eacae6ab2c11341d4a7877bc8dbd9612a15604
agyunoh <uf12@naver.com>
2018-08-21 15:58:10 +0900

    Update modules.py

--- modules.py
+++ modules.py
@@ -36,7 +36,7 @@ class Membrain(nn.Module):
     def __init__(self, opt, num_features,
                  padding_idx=0, start_idx=1, end_idx=2, longest_label=1,
                  num_max_seq, num_layers=6, num_heads=8,
-                 d_model=512, d_k=64, d_v=64,
+                 dim_model=512, d_k=64, d_v=64,
                  proj_share_weight=True, embs_share_weight=True):
 
         super().__init__()
@@ -273,11 +273,11 @@ class Membrain(nn.Module):
 
 class EncoderLayer(nn.Module):
 
-    def __init__(self, d_model, d_inner_hid, n_head, d_k, d_v, dropout=0.1):
+    def __init__(self, dim_model, d_inner_hid, n_head, d_k, d_v, dropout=0.1):
         super(EncoderLayer, self).__init__()
         self.slf_attn = MultiHeadAttention(
-                n_head, d_model, d_k, d_v, dropout=dropout)
-        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner_hid, dropout=dropout)
+                n_head, dim_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForward(dim_model, d_inner_hid, dropout=dropout)
 
     def forward(self, enc_input, slf_attn_mask=None):
         enc_output, enc_slf_attn = self.slf_attn(
@@ -388,11 +388,11 @@ class Encoder(nn.Module):
 
 class DecoderLayer(nn.Module):
 
-    def __init__(self, d_model, d_inner_hid, n_head, d_k, d_v, dropout=0.1):
+    def __init__(self, dim_model, d_inner_hid, n_head, d_k, d_v, dropout=0.1):
         super(DecoderLayer, self).__init__()
-        self.slf_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
-        self.enc_attn = MultiHeadAttention(n_head, d_model, d_k, d_v, dropout=dropout)
-        self.pos_ffn = PositionwiseFeedForward(d_model, d_inner_hid, dropout=dropout)
+        self.slf_attn = MultiHeadAttention(n_head, dim_model, d_k, d_v, dropout=dropout)
+        self.enc_attn = MultiHeadAttention(n_head, dim_model, d_k, d_v, dropout=dropout)
+        self.pos_ffn = PositionwiseFeedForward(dim_model, d_inner_hid, dropout=dropout)
 
     def forward(self, dec_input, enc_output, slf_attn_mask=None, dec_enc_attn_mask=None):
         dec_output, dec_slf_attn = self.slf_attn(
